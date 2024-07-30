93219a47fc91722d101ec2faf898b15a47d5188c
Chetna <120042994+ChetnaShree11@users.noreply.github.com>
2024-07-30 14:27:58 +0530

    Add files via upload

---
+++ 69_leetcode_jwelsStones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(auto i: stones)
+        {
+            size_t j= jewels.find(i);
+            if(j!=string::npos)
+            {
+                count ++;
+            }
+        }
+        return count;
+
+    }
+};
